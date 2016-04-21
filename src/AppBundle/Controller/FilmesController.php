d2b0218f1770caa0a66cbed3d1e258b37095241e
Scheylla Araujo Caetano <scheylla.ac@gmail.com>
2016-04-20 21:48:12 -0300

    Cadastro de Filmes

--- src/AppBundle/Controller/FilmesController.php
+++ src/AppBundle/Controller/FilmesController.php
@@ -4,25 +4,43 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Filmes;
 
-class FilmesController extends Controller
-{
+class FilmesController extends Controller {
+
     /**
      * @Route("/filmes", name="filmes_index")
      */
-    public function indexAction()
-    {
-        return $this->render('filmes/index.html.twig');
+    public function indexAction() {
+
+        $doctrine = $this->getDoctrine()->getEntityManager();
+        $filmes = $doctrine->getRepository('AppBundle:Filmes');
+        
+        $retorno = $filmes->findAll();
+
+        return $this->render('filmes/index.html.twig', 
+                array('filmes'=>$retorno)
+        );
     }
-    
+
     /**
      * @Route("/filmes/cadastro")
      */
-    public function cadastroAction()
-    {
+    public function cadastroAction() {
+        $filme = new Filmes();
+        $filme->setGenero('Terror');
+        $filme->setLancamento(true);
+        $filme->setNome('Rec');
+
+        $doctrine = $this->getDoctrine()->getEntityManager();
+        $doctrine->persist($filme);
+        $doctrine->flush();
+
         return $this->render('filmes/cadastro.html.twig');
     }
+
 }
+
+//doctrine cadastra no banco
